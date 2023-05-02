1c382039c40b633c4b3805e1130a050243734289
Alecpu92 <alessandro_belfiore@hotmail.it>
2023-05-02 17:56:34 +0200

    aggiunta Fizz Buzz

--- js/main.js
+++ js/main.js
@@ -5,16 +5,23 @@ console.log(contenitore);
 // operazioni per quadrati da 1 a 100
 for (let i = 1; i <= 100; i++) {
 
+    for (let i = 1; i < 101; i++) {
+        if (i % 15 == 0) console.log("FizzBuzz");
+        else if (i % 3 == 0) console.log("Fizz");
+        else if (i % 5 == 0) console.log("Buzz");
+        else console.log(i);
+    }
+
     // creazione quadrato
-    const quadrato = document.createElement('span')
+     const quadrato = document.createElement('span')
 
     // classe per ogni quadrato
-    quadrato.classList.add("square");
+     quadrato.classList.add("square");
 
     // valore quadrato
-    quadrato.append(i);
+     quadrato.append(i);
 
     // unione quadrato con contenitore
-    contenitore.append(quadrato);
-    
-    }
+     contenitore.append(quadrato);
+
+     }
